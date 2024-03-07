09763cd5ea12718a98d8493ed04f29005c956e15
simion <catebicini@gmail.com>
2024-03-07 02:15:15 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let addTaskButton = document.querySelector('.add-task-button');
+let tasksContainer = document.querySelector('.tasks-container');
+let input = document.querySelector('.task-input');
+let requiredField = document.querySelector('.required-field');
+
+
+let addTaskFunction = () => {
+
+    requiredField.style.display = 'none';
+
+
+    if (input.value === '') {
+        requiredField.style.display = 'block';
+        return;
+    };
+
+
+    let newTask = document.createElement('li');
+    newTask.style = "cursor: pointer;";
+
+    let p = document.createElement('p');
+    p.innerText = input.value;
+
+    newTask.addEventListener("click", () => { 
+        p.style.textDecoration = "line-through";
+    });
+    
+
+    let img = document.createElement('img');
+    img.setAttribute('src', './images/delete.svg');
+
+    img.addEventListener('click', () => {
+       tasksContainer.removeChild(newTask);
+    });
+
+    newTask.appendChild(p);
+    newTask.appendChild(img);
+
+    tasksContainer.appendChild(newTask);
+
+    // newTask.innerHTML = `
+    //     <p> ${input.value} </p>
+    //     <img src="./images/delete.svg" />
+    // `;
+
+    input.value = '';
+};
+
+addTaskButton.addEventListener('click', addTaskFunction);
